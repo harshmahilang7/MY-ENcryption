e53a396bdb394cc451786f7e84e078dac8d98347
HARSH MAHILANG <harshmahilang7@users.noreply.github.com>
2024-11-26 11:05:17 +0530

    UPDATE

---
+++ VS Encrytions cloud/2023/ultimate-js-course-youtube/01IntroductiontoJs/index.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Dastan_Alam
+ * @Date:   02-07-2023 12:40:52 PM       12:40:52
+ * @Last Modified by:   Dastan_Alam
+ * @Last Modified time: 02-07-2023 12:41:13 PM       12:41:13
+ */
+console.log("Note: View Files from this Repl to Access the Source Code")
+console.log("This program adds 1, 2 and 54")
+
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+let c = addThreeNumbers(1, 2, 54)
+console.log(c)
---
+++ VS Encrytions cloud/2023/ultimate-js-course-youtube/05Chapter1PS/index.js
@@ -0,0 +1,36 @@
+/**
+ * @Author: Dastan_Alam
+ * @Date:   02-07-2023 12:40:52 PM       12:40:52
+ * @Last Modified by:   Dastan_Alam
+ * @Last Modified time: 06-07-2023 12:02:28 PM       12:02:28
+ */
+// Chapter 1 - Q1
+let a = "Harry"
+let b = 6
+console.log(a + b)
+
+// Chapter 1 - Q2
+console.log(typeof (a+b))
+
+// Chapter 1 - Q3
+const a1 = {
+  name: "Harry",
+  section: 1,
+  isPrincipal: false
+}
+// a1 = 45
+// a1 = {}
+
+// Chapter 1 - Q4
+a1['friend'] = "Shubham"  
+a1['name'] = "Lovish"  
+console.log(a1)
+
+// Chapter 1 - Q5
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
