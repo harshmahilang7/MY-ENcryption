8571555b60e4b9a395ae9e2f971e70d7ebd29e3e
HARSH MAHILANG <harshmahilang7@users.noreply.github.com>
2024-12-05 01:26:31 +0530

    Update Disarium.java

--- NARESH_it/3_java/day 28 04-dec-24/day 28/src/com/Disarium/Disarium.java
+++ NARESH_it/3_java/day 28 04-dec-24/day 28/src/com/Disarium/Disarium.java
@@ -4,6 +4,35 @@ public class Disarium {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		Disarium Number 89:> 8^1 + 9^2 = 89
+
+		int num=89;
+		int d=0;
+		int c=num;
+		int sum=0;
+		while(c!=0)
+		{
+			d++;
+			c/=10;
+		}
+		c=num;
+
+
+		while(c!=0)
+		{
+			int rev=c%10;
+			int k=1;
+			for(int i=1;i<=d;i++)
+			{
+				k=k*rev;
+			}
+			d--;
+
+			sum+=k;
+			c/=10;
+		}
+
+		System.out.println(sum);
 
 	}
 
