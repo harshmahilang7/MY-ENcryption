b2bf6d46d2a2a167e474ec69b7c72314f8571943
HARSH MAHILANG <harshmahilang7@users.noreply.github.com>
2024-12-05 09:42:39 +0530

    Create Automorphic_numbers.java

---
+++ NARESH_it/3_java/day 28 04-dec-24/day 28/src/Automorphic_numbers.java
@@ -0,0 +1,27 @@
+
+public class Automorphic_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Automorphic numbers
+
+		//Automorphic numbers. // 5*5=25 // last digit is 5 // 6*6=36 // last digit is 6  // 25*25=625 // last digit is 25
+		int num=25;
+		int sq=num*num;
+		int c=num;
+		int d=0;
+		while(c!=0)
+		{
+			d++;
+			c/=10;
+		}
+
+		for (int i = d; i < sq; i++) {
+
+
+
+		}
+
+	}
+
+}
